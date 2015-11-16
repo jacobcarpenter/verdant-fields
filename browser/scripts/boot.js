598ad4d3e151f86295756f152d4fa48fe3e22c55
Jacob Carpenter <jacobcarpenter@gmail.com>
2015-11-16 13:02:39 -0800

    Remove redundant `game` references.

--- browser/scripts/boot.js
+++ browser/scripts/boot.js
@@ -5,9 +5,9 @@ export default class Boot extends Phaser.State {
 	create() {
 		Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
 
-		this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
-		this.game.scale.setUserScale(4, 4);
-		this.game.scale.refresh();
+		this.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+		this.scale.setUserScale(4, 4);
+		this.scale.refresh();
 
 		this.game.state.start(states.game);
 	}
